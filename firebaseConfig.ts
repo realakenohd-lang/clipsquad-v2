29ad8bdf64fa0ade3008de008feabd32c7d00a88
Akeno <realakenohd@gmail.com>
2025-11-29 22:34:44 -0400

    post images from computer

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,13 +1,13 @@
-// firebaseConfig.ts
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";   // 👈 add this
 
 const firebaseConfig = {
   apiKey: "AIzaSyAyx4RgUj33UZqFPUEcEqvmSknQcEkfQag",
   authDomain: "clipsquad-1b5a2.firebaseapp.com",
   projectId: "clipsquad-1b5a2",
-  storageBucket: "clipsquad-1b5a2.appspot.com",
+  storageBucket: "clipsquad-1b5a2.firebasestorage.app",
   messagingSenderId: "186294550410",
   appId: "1:186294550410:web:332e34ff93827ce23c1273",
 };
@@ -16,3 +16,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);          // 👈 add this
